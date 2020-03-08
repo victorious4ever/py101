07282d3d4f407d534a79219a7c6d20bf92c6b03c
Constantine Mogoreanu <60022702+constmogo@users.noreply.github.com>
2020-03-08 11:16:54 -0700

    Implements tower of Hanoi algorithm

--- towers_of_hanoi.py
+++ towers_of_hanoi.py
@@ -3,13 +3,23 @@
 def pm(n, t1, t2):
     if n == 1:
         print(t1, "->", t2)
-
+        return
+    t3 = 6 - t1 - t2
+    if n == 2:
+        print(t1, "->", t3)
+        print(t1, "->", t2)
+        print(t3, "->", t2)
+        return
+    pm(n - 1, t1, t3)
+    print(t1, "->", t2)
+    pm(n - 1, t3, t2)
+    
 # Prints all the individual moves necessary to
 # transfer n blocks from tower 1 to tower 3.
 def print_moves(n):
-    pm(n, 1, 3)
+        pm(n, 1, 3)
 
-print_moves(1)
+print_moves(4)
 # print_moves(2)
 # 1 -> 2
 # 1 -> 3
